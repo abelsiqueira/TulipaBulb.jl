b51e43838f138c01c89dcebeba500d4a356cbfc2
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2023-12-06 17:00:16 +0100

    Collect sets in the model

--- src/model.jl
+++ src/model.jl
@@ -59,8 +59,8 @@ function create_model(
     end
 
     ## Sets unpacking
-    A = labels(graph)
-    F = edge_labels(graph)
+    A = labels(graph) |> collect
+    F = edge_labels(graph) |> collect
     filter_assets(key, value) = Iterators.filter(a -> getfield(graph[a], key) == value, A)
     filter_flows(key, value) = Iterators.filter(f -> getfield(graph[f...], key) == value, F)
 
